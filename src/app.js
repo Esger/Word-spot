9ece989e42c6e9debb37185cf7a14b9313221e59
Esger <esgerj@gmail.com>
2025-03-29 21:04:35 +0100

    Bak met willekeurige letters

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import $ from 'jquery';
 
 @inject(Element, EventAggregator)
 export class App {
+	title = 'Woord Spot';
 	constructor(element, eventAggregator) {
 		this._element = element;
 		this._eventAggregator = eventAggregator;
 		this._determineTouchDevice();
-		this._element.addEventListener('mousemove', this.mouseMoved.bind(this));
 	}
 
 	_determineTouchDevice() {
