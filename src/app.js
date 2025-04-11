bda75bac8640349daa31c6514d917ed6785b80e9
Esger <esgerj@gmail.com>
2025-04-11 12:46:41 +0200

    refactor to multiplier

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ export class App {
 		this.highScore = this._settingsService.getSettings('high-score') || 0;
 		this._wordSubscription = this._eventAggregator.subscribe('current-word', word => {
 			this.word = this._wordlistService.getText(word);
-			this.score = Math.pow(2, (word.length + this.getBonus(word))) - 1;
+			const multiplier = this.getBonus(word);
+			this.score = Math.pow(2, (word.length + multiplier)) - 1;
 		});
 		this._wordSubmittedSubscription = this._eventAggregator.subscribe('word-submitted', success => {
 			this.count += success;
