4257ab005ecdc200475d2943b3f6df785e98323a
Esger <esgerj@gmail.com>
2025-04-27 02:43:32 +0200

    help menu

--- src/resources/elements/menu.js
+++ src/resources/elements/menu.js
@@ -1,1 +1,13 @@
-export class Menu { }
+import { inject, bindable } from 'aurelia-framework';
+@inject(Element)
+export class MenuCustomElement {
+	@bindable targetPopover;
+	@bindable menuIcon;
+	constructor(element) {
+		this._element = element;
+	}
+	attached() {
+		$(this._element).find('.menuToggle').attr('popovertarget', this.targetPopover);
+		$(this._element).find('[popover]').attr('id', this.targetPopover);
+	}
+}
